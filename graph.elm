90cfb8cd9b20728d34049e509f6a2ef081fd43a9
Christian Peterek <christian.peterek@brainlab.com>
2018-06-26 09:23:08 +0200

    Ran elm-format

--- graph.elm
+++ graph.elm
@@ -2,99 +2,113 @@ module Graph exposing (..)
 
 import StaticData exposing (..)
 
+
 type alias Graph =
-  {  nodes : List Node
-  ,  edges : List Edge
-  }
-
-type GraphPath = GraphPath
-  { node : Node
-  , sub  : List GraphPath
-  }
+    { nodes : List Node
+    , edges : List Edge
+    }
 
 
-filterGraph : Graph -> String -> Graph 
-filterGraph graph id = 
-  let
-    ids =
-      findIdsToBottom graph [id]    
-  in
-    {
-      nodes = List.filter (\n -> List.member n.id ids ) graph.nodes
-    , edges = List.filter (\n -> List.member n.from ids ) graph.edges
-  }
+type GraphPath
+    = GraphPath
+        { node : Node
+        , sub : List GraphPath
+        }
+
+
+filterGraph : Graph -> String -> Graph
+filterGraph graph id =
+    let
+        ids =
+            findIdsToBottom graph [ id ]
+    in
+        { nodes = List.filter (\n -> List.member n.id ids) graph.nodes
+        , edges = List.filter (\n -> List.member n.from ids) graph.edges
+        }
+
 
 findIdsToBottom : Graph -> List String -> List String
 findIdsToBottom graph ids =
-  let
-    found = 
-      List.filter (\e -> List.member e.from ids) graph.edges |>
-        List.map (\e -> e.to)
+    let
+        found =
+            List.filter (\e -> List.member e.from ids) graph.edges
+                |> List.map (\e -> e.to)
 
-    bottom = "VISION"
+        bottom =
+            "VISION"
 
-    bottomReached = List.member bottom found
-  in
-    if bottomReached then 
-      List.append ids found
-    else
-      findIdsToBottom graph found |> List.append ids
+        bottomReached =
+            List.member bottom found
+    in
+        if bottomReached then
+            List.append ids found
+        else
+            findIdsToBottom graph found |> List.append ids
 
 
 dddrr : List String -> String -> List String -> Bool
 dddrr list needle =
-  List.any (\a -> a == needle ) 
+    List.any (\a -> a == needle)
 
 
 ddd : List String -> String -> List String -> Bool
 ddd list needle =
-  List.any (\a -> a == needle ) 
+    List.any (\a -> a == needle)
 
 
 pathify : Graph -> Node -> GraphPath
 pathify graph node =
-  let
-    findNodesFromGraph = findNodesByEdge graph
-    pathifyGraph = pathify graph
-  in
+    let
+        findNodesFromGraph =
+            findNodesByEdge graph
 
-  GraphPath 
-    { node = node
-    , sub = findEdgesByNode graph node |> 
-            List.concatMap findNodesFromGraph |> 
-            List.map pathifyGraph
-  } 
+        pathifyGraph =
+            pathify graph
+    in
+        GraphPath
+            { node = node
+            , sub =
+                findEdgesByNode graph node
+                    |> List.concatMap findNodesFromGraph
+                    |> List.map pathifyGraph
+            }
+
 
 findNodeById : Graph -> String -> Maybe Node
 findNodeById graph id =
-  findNodesById graph id |>
-    List.head
+    findNodesById graph id
+        |> List.head
+
 
 findNodesById : Graph -> String -> List Node
 findNodesById graph id =
-  List.filter (\n -> n.id == id) graph.nodes
+    List.filter (\n -> n.id == id) graph.nodes
+
 
 findNodesByEdge : Graph -> Edge -> List Node
 findNodesByEdge graph edge =
-  let
-    edgeConnectsTo = connectsTo edge
-  in
-    List.filter edgeConnectsTo graph.nodes
+    let
+        edgeConnectsTo =
+            connectsTo edge
+    in
+        List.filter edgeConnectsTo graph.nodes
+
 
 findEdgesByNode : Graph -> Node -> List Edge
 findEdgesByNode graph node =
-  let
-    nodeConnectsFrom : Edge -> Bool
-    nodeConnectsFrom = connectsFrom node
-  in
-    List.filter nodeConnectsFrom graph.edges      
+    let
+        nodeConnectsFrom : Edge -> Bool
+        nodeConnectsFrom =
+            connectsFrom node
+    in
+        List.filter nodeConnectsFrom graph.edges
+
 
 connectsTo : Edge -> Node -> Bool
 connectsTo edge node =
-  edge.to == node.id
+    edge.to == node.id
 
-connectsFrom : Node -> Edge -> Bool 
+
+connectsFrom : Node -> Edge -> Bool
 connectsFrom node edge =
-  edge.from == node.id
-
-
+    edge.from == node.id
--- main.elm
+++ main.elm
@@ -1,6 +1,9 @@
 -- Read more about this program in the official Elm guide:
 -- https://guide.elm-lang.org/architecture/effects/time.html
 
+
+module Main exposing (..)
+
 import Html exposing (Html)
 import Html exposing (..)
 import Html.Attributes as HAttr exposing (..)
@@ -8,100 +11,110 @@ import Svg exposing (svg, rect)
 import Svg.Attributes as SAttr exposing (..)
 import Time exposing (Time, second)
 import List exposing (..)
-
 import StaticData exposing (..)
 import Graph exposing (..)
 
+
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
 
 -- MODEL
 
-type alias Model = Graph
+
+type alias Model =
+    Graph
 
 
-init : (Model, Cmd Msg)
+init : ( Model, Cmd Msg )
 init =
-  let
-    default : Graph
-    default = 
-      { nodes = projects 
-      , edges = connections
-      }
-  in
-    
-  (default, Cmd.none)
+    let
+        default : Graph
+        default =
+            { nodes = projects
+            , edges = connections
+            }
+    in
+        ( default, Cmd.none )
+
+
 
 -- UPDATE
 
+
 type Msg
-  = Tick Time
+    = Tick Time
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Tick newTime ->
-      (model, Cmd.none)
+    case msg of
+        Tick newTime ->
+            ( model, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+    Sub.none
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  let
-    node = Maybe.withDefault { id = "NC1", name = "?" } <| 
-      findNodeById model "NC1"
+    let
+        node =
+            Maybe.withDefault { id = "NC1", name = "?" } <|
+                findNodeById model "NC1"
 
-    bgg = Graph.filterGraph model "NC5"
-    bff = List.map (\a -> a.id) bgg.nodes |> 
-      Debug.log "debug 70:" 
+        bgg =
+            Graph.filterGraph model "NC5"
 
+        bff =
+            List.map (\a -> a.id) bgg.nodes
+                |> Debug.log "debug 70:"
 
-    
-  
-    path : GraphPath
-    path = pathify model node
-  in
+        path : GraphPath
+        path =
+            pathify model node
+    in
+        div []
+            [ renderPath path
+            , svg [ SAttr.width "500", SAttr.height "500", SAttr.viewBox "0 0 500 500" ]
+                [ rect [ SAttr.x "10", SAttr.y "10", SAttr.width "100", SAttr.height "100", SAttr.rx "15", SAttr.ry "15" ] []
+                ]
+            ]
 
-    div [] [
-        renderPath path
-      , svg [ SAttr.width "500", SAttr.height "500", SAttr.viewBox "0 0 500 500" ] [ 
-          rect [ SAttr.x "10", SAttr.y "10", SAttr.width "100", SAttr.height "100", SAttr.rx "15", SAttr.ry "15" ] [] 
-        ]
-      ]
 
 renderPath : GraphPath -> Html msg
 renderPath path =
-  let
-    (GraphPath graphPath) = path
-  in
-
-  div [ HAttr.style [("marginLeft", "10px")] ] [
-    text (graphPath.node.name ++ "(" ++ graphPath.node.id ++ ")")
-    , div [] <| List.map renderPath graphPath.sub
-   ]
+    let
+        (GraphPath graphPath) =
+            path
+    in
+        div [ HAttr.style [ ( "marginLeft", "10px" ) ] ]
+            [ text (graphPath.node.name ++ "(" ++ graphPath.node.id ++ ")")
+            , div [] <| List.map renderPath graphPath.sub
+            ]
 
 
 render : List Node -> String
 render nodes =
-  case head nodes of
-    Maybe.Nothing ->
-      "--"
-    Maybe.Just node ->
-      node.name
+    case head nodes of
+        Maybe.Nothing ->
+            "--"
 
-
-
+        Maybe.Just node ->
+            node.name
